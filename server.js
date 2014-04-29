b7710139ad746e00ce3e26da8159a21847c43c26
Greg Little <glittle@gmail.com>
2014-04-29 17:28:28 -0500

    using ws instead of sockjs, and making billboard work differently (each socket gets to have one message on the billboard while they’re there)

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 
-console.log('going to get going here')
-
 var _ = require('./index.js')
 require('./server_utils.js')
 try { require('./_config.js') } catch (e) {}
@@ -8,65 +6,52 @@ try { require('./_config.js') } catch (e) {}
 _.run(function () {
     if (!process.env.PORT) process.env.PORT = 8080
 
-    var app = require('express.io')()
-    app.http().io()
-
-    ///
-
-    var billboard = {}
-
-    function update(b) {
-        var now = _.time()
-        _.each(b, function (b, key) {
-            if (b.if && b.if != billboard[key].data) return
-            if (b.delete) {
-                delete billboard[key]
-            } else {
-                billboard[key] = {
-                    data : b.data,
-                    time : now
-                }
-            }
-        })
-        billboard = _.filter(billboard, function (b) { return b.time > now - 1000*60*60 })
-    }
-
-    app.io.route('read', function(req) {
-        req.io.emit('billboard', billboard)
-    })
-
-    app.io.route('write', function(req) {
-        update(req.data)
-        app.io.broadcast('billboard', billboard)
-    })
-
-    ///
+    var app = require('express')()
 
     var OpenTok = require('opentok')
     var opentok = new OpenTok.OpenTokSDK(process.env.OPENTOK_KEY, process.env.OPENTOK_SECRET)
 
-    app.io.route('createSession', function (req) {
+    app.post('/createSession', function (req, res) {
         _.run(function () {
-            req.io.respond(_.p(opentok.createSession('127.0.0.1', _.p())))
+            corsSend(req, res, _.p(opentok.createSession('127.0.0.1', _.p())))
         })
     })
 
-    app.io.route('createToken', function (req) {
-        req.io.respond(opentok.generateToken({
-            session_id : req.data,
+    app.post('/createToken', function (req, res) {
+        corsSend(req, res, opentok.generateToken({
+            session_id : req.body,
             role : OpenTok.RoleConstants.PUBLISHER,
             expire_time : _.time() + 1000*60*30,
             connection_data : "hello world"
         }))
     })
 
-    ///
-
-    app.get('/', function(req, res) {
-        res.sendfile(__dirname + '/index.html')
+    app.get('/', function (req, res) {
+        res.sendfile(__dirname + '/index.html');
     })
 
-    app.listen(process.env.PORT, function() {
+    var server = require('http').createServer(app)
+    server.listen(process.env.PORT, function () {
         console.log("go to http://localhost:" + process.env.PORT)
+    })
+
+    sockets = {}
+    billboard = {}
+    var ws = new (require('ws').Server)({ server : server })
+    ws.on('connection', function (s) {
+        var id = Math.random()
+        sockets[id] = s
+        billboard[id] = null
+        s.on('close', function () {
+            delete sockets[id]
+            delete billboard[id]
+        })
+
+        s.on('message', function (data) {
+            if (!data) return s.send(_.json(billboard))
+            billboard[id] = _.unJson(data)
+            var b = _.json(billboard)
+            _.each(sockets, function (s) { s.send(b) })
+        })
     })
 })
--- server_utils.js
+++ server_utils.js
@@ -7,3 +7,21 @@ process.on('uncaughtException', function (err) {
         console.log(err.stack)
     } catch (e) {}
 })
+
+corsSend = function (req, res, body) {
+    var mime = 'text/plain'
+    if (typeof body != 'string') {
+        body = _.json(body)
+        mime = 'application/json'
+    }
+    var headers = {
+        'Content-Type': mime + '; charset=utf-8',
+        'Content-Length': Buffer.byteLength(body)
+    }
+    if (req.headers.origin) {
+        headers['Access-Control-Allow-Origin'] = req.headers.origin
+        headers['Access-Control-Allow-Credentials'] = 'true'
+    }
+    res.writeHead(200, headers)
+    res.end(body)
+}
