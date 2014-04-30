2af286b0d947608c75c21f74b5fe2644c3b0c20e
Greg Little <glittle@gmail.com>
2014-04-29 20:54:40 -0500

    enabled p2p

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ _.run(function () {
 
     app.post('/createSession', function (req, res) {
         _.run(function () {
-            corsSend(req, res, _.p(opentok.createSession('127.0.0.1', _.p())))
+            corsSend(req, res, _.p(opentok.createSession('127.0.0.1', { 'p2p.preference' : 'enabled' }, _.p())))
         })
     })
 
